80d386777675ef6a12361d2dbd5b5400f9a3a393
Dmitriy <sctoss@gmail.com>
2022-10-10 13:59:45 +0300

    Completed tasks

--- Seminar5/task01.py
+++ Seminar5/task01.py
@@ -6,13 +6,15 @@ class color:
     BOLD = '\033[1m'
     END = '\033[0m'
 
-k = 202
+k = 2021 # конфеты
 m = 28
-print(f'Первому игроку нужно взять {k % (m + 1)} конфет')
+print(f'Первому игроку нужно взять {k % (m + 1)} конфет') # ответ на вопрос в задаче
 
+#Разработка игры
 igrok1 = input('Введите имя игрок №1: ')
 igrok2 = 'Bot'
 
+# жребий для игрока и бота
 while True:
     print(f'{igrok1}, введите число от 1 до 100: ')
     igrok1_digit = int(input())
@@ -25,31 +27,31 @@ while True:
         print(f'Первым ходит {igrok2}')
         break
     else:
-        igrok1_digit == igrok2_digit == False
+        igrok1_digit == igrok2_digit == False # вариант при котором у обоих одинаковые значения
 
 count = 1
 print('Введите число от 1 до 28: ')
 while k != 0:
     print(f'\n{color.BOLD}Ход №{count}: {color.END}')
-    player1 = -1
-    while player1 <= 0 or player1 >= 29:
+    player1 = -1 # значение для входа в цикл
+    while player1 <= 0 or player1 >= 29: # проверка на ограничение во взятии конфет от 1 до 28
         player1 = int(input('Игрок №1: '))
-    k = k - player1
+    k = k - player1 # уменьшаем общее количество конфет
     print(f'{color.RED}Конфет осталось {k}{color.END}')
     if k == 0:
         print(color.GREEN + 'Игрок №1 Победил!' + color.END)
         break
-    if k <= 28:
+    if k <= 28: # наделяем интелектом бота, когда меньше 28, чтобы забирал все что есть
         player2 = k
         print(f'Игрок №2: {player2}')
         k = k - player2
         print(f'{color.RED}Конфет осталось {k}{color.END}')
         print(color.GREEN + 'Игрок №2 Победил!' + color.END)
         break
-    player2 = int(random.randint(1, 28))
+    player2 = int(random.randint(1, 28)) # ход бота
     print(f'Игрок №2: {player2}')
-    k = k - player2
+    k = k - player2 # уменьшаем общее количество конфет
     print(f'{color.RED}Конфет осталось {k}{color.END}')
     if k == 0:
         print(color.GREEN + 'Игрок №2 Победил!' + color.END)
-    count += 1
+    count += 1 # цикл для количество Ходов
